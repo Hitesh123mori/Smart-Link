5a5106a7277383d615200fee89eb609263f14db4
Niraj kc <niraj.kc.128@gmail.com>
2024-03-03 08:43:32 +0530

    Chat model update

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,32 +1,43 @@
+import 'package:ingenious_5/apis/FirebaseAPIs.dart';
+
 class DoubtMessage{
   DoubtMessage({
     required this.msg,
     required this.type,
     required this.fromId,
+    required this.fromName,
     required this.time,
     required this.vote,
-  });
+  }){
+    cId = FirebaseAPIs.uuid.v1();
+  }
   late final String msg;
   late final Type type;
   late final String fromId;
   late final String time;
   late final num vote;
+  late final String fromName;
+  late final String cId;
 
   DoubtMessage.fromJson(Map<String, dynamic> json){
+    cId = json['cId'].toString();
     msg = json['msg'].toString();
     type = json['type'].toString() == Type.image.name ? Type.image  :Type.text;
     fromId = json['fromId'].toString();
     time = json['time'].toString();
     vote = json['vote'];
+    fromName = json['fromName'];
   }
 
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
+    data['cId'] = cId;
     data['msg'] = msg;
     data['type'] = type.name;
     data['fromId'] = fromId;
     data['time'] = time ;
     data['vote'] = vote;
+    data['fromName'] = fromName;
     return data;
   }
 }
